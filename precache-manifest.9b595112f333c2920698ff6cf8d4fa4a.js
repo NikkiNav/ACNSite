1bd40f2535d45835a8b3ddae4dbb5113e64d7a06
Aliya Rahman <aliya.rahman@yahoo.com>
2018-12-02 17:45:24 -0500

    Updates

---
+++ precache-manifest.9b595112f333c2920698ff6cf8d4fa4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "94978012c143b86d380fa4dcdd54eafb",
+    "url": "/static/media/lvf-three-heads_web.94978012.jpg"
+  },
+  {
+    "revision": "aa4e43ac07a17e8544cfb5a9bfb5ce2e",
+    "url": "/static/media/acnsig_sm.aa4e43ac.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "322c004c0db74d131219",
+    "url": "/static/js/main.322c004c.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "322c004c0db74d131219",
+    "url": "/static/css/main.cd3ac867.chunk.css"
+  },
+  {
+    "revision": "ae6b0fe430ce8f4a3eb67b2c64253d47",
+    "url": "/index.html"
+  }
+];
